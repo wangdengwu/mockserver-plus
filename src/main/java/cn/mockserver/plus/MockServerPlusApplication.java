8d82b1fdcddff09e007c50108c22bb18e2d24ed5
wangdengwu <dengwu.wang@gmail.com>
2021-06-24 02:02:41 +0800

    ready for dev

---
+++ src/main/java/cn/mockserver/plus/MockServerPlusApplication.java
@@ -0,0 +1,13 @@
+package cn.mockserver.plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockServerPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockServerPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/mockserver/plus/MockServerPlusTestApplication.java
@@ -0,0 +1,13 @@
+package cn.mockserver.plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockServerPlusTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockServerPlusTestApplication.class, args);
+	}
+
+}
