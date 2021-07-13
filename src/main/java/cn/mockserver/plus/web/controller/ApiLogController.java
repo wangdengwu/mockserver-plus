120170652a8a2e460695b9f5c77133bbe75e5b64
wangdengwu <dengwu.wang@gmail.com>
2021-07-13 16:51:42 +0800

    更新UI

--- src/main/java/cn/mockserver/plus/web/controller/ApiLogController.java
+++ src/main/java/cn/mockserver/plus/web/controller/ApiLogController.java
@@ -2,7 +2,9 @@ package cn.mockserver.plus.web.controller;
 
 import cn.mockserver.plus.interceptor.MockServerInterceptor;
 import cn.mockserver.plus.web.view.ResultAjax;
+import org.apache.commons.lang3.StringUtils;
 import org.mockserver.model.HttpRequest;
+import org.mockserver.serialization.RequestDefinitionSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +21,12 @@ public class ApiLogController extends BaseController {
     private MockServerInterceptor mockServerInterceptor;
 
     @PostMapping("/query")
-    public ResultAjax query(@RequestParam String path) {
+    public ResultAjax query(@RequestParam(required = false) String path) {
         HttpRequest httpRequest = HttpRequest.request(path);
+        httpRequest.withQueryStringParameter("type", "REQUEST_RESPONSES");
+        if (StringUtils.isNotBlank(path)) {
+            httpRequest.withBody(new RequestDefinitionSerializer(null).serialize(HttpRequest.request(path)));
+        }
         return success("ok", mockServerInterceptor.retrieve(httpRequest));
     }
 }
